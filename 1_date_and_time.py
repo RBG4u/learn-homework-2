996cd9e58a9be2b53124a14a114551eaf15b6893
Vladislav Samkov <8867sa12@gmail.com>
2023-02-23 20:12:10 +0300

    Complet

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -1,27 +1,14 @@
-"""
-Домашнее задание №2
-
-Дата и время
-
-1. Напечатайте в консоль даты: вчера, сегодня, 30 дней назад
-2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
-
-"""
+from datetime import datetime, timedelta
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    dt_now = datetime.now().date()
+    delta_1 = timedelta(days=1)
+    delta_30 = timedelta(days=30)
+    print(f"Сегодня: {dt_now.strftime('%d.%m.%Y')} \nВчера: {(dt_now - delta_1).strftime('%d.%m.%Y')} \n30 дней назад: {(dt_now - delta_30).strftime('%d.%m.%Y')}")
 
-
-def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+def str_2_datetime(str_date):
+    date = datetime.strptime(str_date, "%d/%m/%y %H:%M:%S.%f")
+    return date
 
 if __name__ == "__main__":
     print_days()
--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -1,21 +1,21 @@
-"""
+import csv
 
-Домашнее задание №2
-
-Работа csv
-
-1. Создайте список словарей с ключами name, age и job и значениями по вашему выбору. 
-   В списке нужно создать не менее 4-х словарей
-2. Запишите содержимое списка словарей в файл в формате csv
-
-"""
+users_card = [
+        {'name': 'Маша', 'age': 25, 'gender': 'female', 'job': 'Scientist'}, 
+        {'name': 'Вася', 'age': 8, 'gender': 'male', 'job': 'Programmer'}, 
+        {'name': 'Эдуард', 'age': 48, 'gender': 'male', 'job': 'Big boss'},
+        {'name': 'Катя', 'age': 33, 'gender': 'female', 'job': 'Manager'},
+        {'name': 'Никита', 'age': 56, 'gender': 'male', 'job': 'Digger'}
+    ]
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    with open ('users.csv', 'w', encoding='utf-8') as users:
+        fields = ['name', 'age', 'gender', 'job']
+        writer = csv.DictWriter(users, fields, delimiter=';')
+        writer.writeheader()
+        for user in users_card:
+            writer.writerow(user)
+
 
 if __name__ == "__main__":
     main()
